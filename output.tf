c16eccf613abc73dd1b10705701c1873f83cddcb
Avi Cavale <avi@shippable.com>
2018-05-29 03:04:13 +0530

    commenting ALB code

--- output.tf
+++ output.tf
@@ -10,10 +10,10 @@ output "ecs_instance_role-name" {
   value = "${aws_iam_role.ecs_instance_role.name}"
 }
 
-output "ecsLoadBalancer-name" {
-    value = "${aws_alb.ecsLoadBalancer.name}"
-}
-
-output "ecsTargetGroup-arn" {
-    value = "${aws_alb_target_group.ecsTargetGroup.arn}"
-}
+//output "ecsLoadBalancer-name" {
+//    value = "${aws_alb.ecsLoadBalancer.name}"
+//}
+//
+//output "ecsTargetGroup-arn" {
+//    value = "${aws_alb_target_group.ecsTargetGroup.arn}"
+//}
